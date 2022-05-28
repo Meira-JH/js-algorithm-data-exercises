fce15392dcd4689112a935af2237151f70ead341
Meira-JH <jhalvesmeira@gmail.com>
2022-05-28 12:56:33 -0300

    feat: isPalindrome with pointers

--- src/3-problem-solving-patterns/pointerPatterns.js
+++ src/3-problem-solving-patterns/pointerPatterns.js
@@ -192,3 +192,15 @@ export function containsInOrder(input1, input2) {
 
     return false;
 }
+
+export function isPalindrome(string) {
+    let firstPointer = 0;
+    let secondPointer = string.length - 1;
+
+    while (firstPointer + 1 <= string.length / 2) {
+        if (string[firstPointer] !== string[secondPointer]) return false;
+        ++firstPointer;
+        --secondPointer;
+    }
+    return true;
+}
---
+++ src/5-extra-problems/isPalindrome.js
@@ -0,0 +1,11 @@
+export function isPalindrome(string) {
+    let firstPointer = 0;
+    let secondPointer = string.length - 1;
+
+    while (firstPointer + 1 <= string.length / 2) {
+        if (string[firstPointer] !== string[secondPointer]) return false;
+        ++firstPointer;
+        --secondPointer;
+    }
+    return true;
+}
