d0a17bf77bc6c1289b85166edb439dc026ed156d
Meira-JH <jhalvesmeira@gmail.com>
2022-05-29 20:00:52 -0300

    feat: flatten array of arrays

---
+++ src/5-extra-problems/flattenArray.js
@@ -0,0 +1,16 @@
+//write a function that transforms a array of arrays
+//into an array of elements (only one depth)
+export function flatten(arrayOfArrays) {
+    let flattenArray = [];
+
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        if (typeof arrayOfArrays[i] !== "object") {
+            flattenArray = [...flattenArray, arrayOfArrays[i]];
+        }
+        if (typeof arrayOfArrays[i] === "object") {
+            flattenArray = flattenArray.concat(flatten(arrayOfArrays[i]));
+        }
+    }
+
+    return flattenArray;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { subarrayLengthSumIsEqualOrGreater } from "./3-problem-solving-patterns/slidingWindow";
 import { productOfAll } from "./4-recurssion/2-helperMethod";
 import { productOfArray } from "./4-recurssion/3-pureRecursion";
+import { flatten } from "./5-extra-problems/flattenArray";
 import { reverseString } from "./5-extra-problems/reverseString";
 import { someRecursive } from "./5-extra-problems/someRecursive";
 import { linearSearch } from "./6-searching-algorithms/linearSearch";
@@ -8,6 +9,7 @@ import { linearSearch } from "./6-searching-algorithms/linearSearch";
 const array1 = [-1, -2, -2, 2, 4, 4, 4, 5, 6, 7, 7, 7, 9, 10];
 const array2 = [-1, -2, 2, 4, 5, 6, 7, 9, 10];
 const array3 = [2, 4, 6];
+const arrayOfArrays1 = [1, 2, [2, 3], [7, 5, 24], 0, [0, 0, 1]];
 const number1 = 123123121231231212312312222;
 const number2 = 998890099988900999889009333;
 const number3 = 321;
@@ -15,6 +17,4 @@ const number4 = 123;
 const string1 = "abc";
 const string2 = "abracadabra";
 
-const isOdd = (val) => val % 2 !== 0;
-
-console.log(someRecursive(array1, isOdd));
+console.log(flatten(arrayOfArrays1));
