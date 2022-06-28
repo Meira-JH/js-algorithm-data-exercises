96bd60b46ce4997cfac5736346b4b0864faefde2
Meira-JH <jhalvesmeira@gmail.com>
2022-06-28 10:04:27 -0300

    feat: singly list comments

--- src/dataStructures/singlyLinkedList.js
+++ src/dataStructures/singlyLinkedList.js
@@ -1,4 +1,5 @@
 class Node {
+    //defines value of node and next node
     constructor(value) {
         this.value = value;
         this.next = null;
@@ -6,22 +7,29 @@ class Node {
 }
 
 export class SinglyLinkedList {
+    //defines head, tail and length
     constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
+    //defines the method to add new elements to the end of list
     push(value) {
+        //instantiate new node
         const newNode = new Node(value);
+        //if there is no head, make it head
         if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
+            //else adds new element to end of list
+            //and defines the new element as tail
         } else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
 
+        //every push adds 1 to length
         ++this.length;
         return this;
     }
