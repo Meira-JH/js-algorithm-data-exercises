29a5b49f8f5267d0bc0bd855584c1dfeac4b58eb
Meira-JH <jhalvesmeira@gmail.com>
2022-07-13 18:39:57 -0300

    feat: queue structure enqueue and dequeue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import { Stack } from './dataStructures/stack'
+import { Queue } from './dataStructures/queue';
 
-const stack = new Stack
 
-stack.push('lol')
-stack.push('omg')
-stack.push('wtf')
-stack.pop()
-stack.pop()
-stack.pop()
-console.log(stack)
+const q = new Queue
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+console.log(q)
