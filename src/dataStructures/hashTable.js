4c8f1c7be767d6ac080947e07368366f789ccf9e
Meira-JH <jhalvesmeira@gmail.com>
2022-08-12 10:56:43 -0300

    feat: set improvmnt, get and values

--- src/dataStructures/hashTable.js
+++ src/dataStructures/hashTable.js
@@ -33,15 +33,59 @@ export class HashTable {
     }
 
     set(key, value) {
-        let hashedIndex = this.hash(key);
+        const hashedIndex = this.hash(key);
 
         //check if the position to be inserted in array
-        //has any element, if not insert a
+        //has any element, if not insert an empty array to be nested
         if (!this.keyMap[hashedIndex]) {
             this.keyMap[hashedIndex] = [];
         }
-        this.keyMap[hashedIndex].push({ [key]: value });
+        // insert nested elements inside an index
+        if (this.keyMap[hashedIndex].length > 0) {
+            for (let [index, element] of this.keyMap[hashedIndex].entries()) {
+                if (element.key === key || element.value === value) {
+                    element = { key, value };
+                } else if (index === this.keyMap[hashedIndex].length - 1) {
+                    this.keyMap[hashedIndex].push({ key, value });
+                }
+            }
+        } else {
+            this.keyMap[hashedIndex].push({ key, value });
+        }
 
-        return console.log(hashedIndex);
+        return hashedIndex;
+    }
+
+    get(key) {
+        const hashedIndex = this.hash(key);
+
+        if (!this.keyMap[hashedIndex][0]) {
+            return null;
+        }
+
+        for (const element of this.keyMap[hashedIndex]) {
+            if (element[key]) {
+                return element;
+            }
+        }
+
+        return null;
+    }
+
+    values() {
+        let values = [];
+
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (const element of this.keyMap[i]) {
+                    //avoid duplicate values
+                    if (!values.includes(element.value)) {
+                        values.push(element.value);
+                    }
+                }
+            }
+        }
+
+        return values;
     }
 }
